3de9399a7241a9cbed3062bbc26925ca8fac7d25
qianzd <qian.wolf@gmail.com>
2016-01-14 16:51:55 +0800

    web端页面改版完成，增加滑动效果

--- src/html/js/webindex.js
+++ src/html/js/webindex.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-	$("#logoImgBig").fadeIn();
-	$("#mainMenu").fadeIn();
-	$("#content").fadeIn();
+	$("body").fadeIn();
 	var currentPage=0;
-	// var animPage=null;
+	var animPage=null;
+	var changeStop=true;
 
 	$("#flashBtn").click(function(){changePage(1)});
 	$("#androidBtn").click(function(){changePage(2)});
@@ -18,65 +17,75 @@ $(document).ready(function(){
 	//test
 
 	function changePage(index){
-		if(currentPage==0){
-			$("#page0").addClass("moveoutclass");
-			$("#page0").addEventListener("webkitAnimationEnd", function() {      alert("end");  });
-			$("#logoImgBig").slideUp();
-			$("#logoImgSmall").slideDown();
-		}else{
-			switch(currentPage){
-				case 1:
-				$("#flashBtn").removeClass("mainMenuBtnchoiced");
-				$("#page1").removeClass("moveinclass");
-				$("#page1").addClass("moveoutclass");
-				var animPage=$("#page1");
-				animPage.addEventListener("webkitAnimationEnd", animationListener);
-				break;
-				case 2:
-				$("#androidBtn").removeClass("mainMenuBtnchoiced");
-				$("#page2").removeClass("moveinclass");
-				$("#page2").addClass("moveoutclass");
-				break;
-				case 3:
-				$("#paintingBtn").removeClass("mainMenuBtnchoiced");
-				$("#page3").removeClass("moveinclass");
-				$("#page3").addClass("moveoutclass");
-				break;
-				case 4:
-				$("#aboutBtn").removeClass("mainMenuBtnchoiced");
-				$("#page4").removeClass("moveinclass");
-				$("#page4").addClass("moveoutclass");
-				break;
+		if(changeStop){
+			changeStop=false;
+			if(currentPage==0){
+				animPage=$("#page0");
+				animPage.addClass("moveoutclass");
+				animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
+				$("#logoImgBig").slideUp();
+				$("#logoImgSmall").slideDown();
+			}else{
+				switch(currentPage){
+					case 1:
+						$("#flashBtn").removeClass("mainMenuBtnchoiced");
+						$("#page1").removeClass("moveinclass");
+						animPage=$("#page1");
+						animPage.addClass("moveoutclass");
+						animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
+						break;
+					case 2:
+						$("#androidBtn").removeClass("mainMenuBtnchoiced");
+						$("#page2").removeClass("moveinclass");;
+						animPage=$("#page2");
+						animPage.addClass("moveoutclass");
+						animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
+						break;
+					case 3:
+						$("#paintingBtn").removeClass("mainMenuBtnchoiced");
+						$("#page3").removeClass("moveinclass");
+						animPage=$("#page3");
+						animPage.addClass("moveoutclass");
+						animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
+						break;
+					case 4:
+						$("#aboutBtn").removeClass("mainMenuBtnchoiced");
+						$("#page4").removeClass("moveinclass");;
+						animPage=$("#page4");
+						animPage.addClass("moveoutclass");
+						animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
+						break;
+				}
 			}
+			switch(index){
+				case 1:
+					$("#flashBtn").addClass("mainMenuBtnchoiced");
+					$("#page1").show();
+					$("#page1").addClass("moveinclass");
+					break;
+				case 2:
+					$("#androidBtn").addClass("mainMenuBtnchoiced");
+					$("#page2").show();
+					$("#page2").addClass("moveinclass");
+					break;
+				case 3:
+					$("#paintingBtn").addClass("mainMenuBtnchoiced");
+					$("#page3").show();
+					$("#page3").addClass("moveinclass");
+					break;
+				case 4:
+					$("#aboutBtn").addClass("mainMenuBtnchoiced");
+					$("#page4").show();
+					$("#page4").addClass("moveinclass");
+					break;
+			}
+			currentPage=index;
 		}
-		switch(index){
-			case 1:
-			$("#flashBtn").addClass("mainMenuBtnchoiced");
-			$("#page1").show();
-			$("#page1").addClass("moveinclass");
-			break;
-			case 2:
-			$("#androidBtn").addClass("mainMenuBtnchoiced");
-			$("#page2").show();
-			$("#page2").addClass("moveinclass");
-			break;
-			case 3:
-			$("#paintingBtn").addClass("mainMenuBtnchoiced");
-			$("#page3").show();
-			$("#page3").addClass("moveinclass");
-			break;
-			case 4:
-			$("#aboutBtn").addClass("mainMenuBtnchoiced");
-			$("#page4").show();
-			$("#page4").addClass("moveinclass");
-			break;
-		}
-		currentPage=index;
 	}
 
 	function animationListener(){
-		alert("end");
-	    $("#page0").removeClass("moveoutclass");
-	    $("#page0").hide();
+		animPage.removeClass("moveoutclass");
+		animPage.hide();
+		changeStop=true;
 	}
 });
