3043b542beeb99ebaed1f014846ffa5e550ec026
qianzd <qian.wolf@gmail.com>
2016-01-27 16:46:20 +0800

    移动端页面增加手势切换

--- src/html/js/mindex.js
+++ src/html/js/mindex.js
@@ -1,88 +1,91 @@
 $(document).ready(function(){
 	$("body").fadeIn();
 	var currentPage=0;
-	var animPage=null;
-	var changeStop=true;
+	var pageI;
 
-	$("#flashBtn").click(function(){changePage(1)});
-	$("#androidBtn").click(function(){changePage(2)});
-	$("#paintingBtn").click(function(){changePage(3)});
-	$("#aboutBtn").click(function(){changePage(4)});
+	$("#flashBtn").click(function(){myChangePage(1,false)});
+	$("#androidBtn").click(function(){myChangePage(2,false)});
+	$("#paintingBtn").click(function(){myChangePage(3,false)});
+	$("#aboutBtn").click(function(){myChangePage(4,false)});
 
-	$("#page1").hide();
-	$("#page2").hide();
-	$("#page3").hide();
-	$("#page4").hide();
-
+	$("#content").bind('swiperight', function() {
+		if (currentPage == 0 || currentPage == 1) {
+			pageI = 4;
+		} else {
+			pageI = currentPage - 1;
+		}
+		myChangePage(pageI);
+	}).bind('swipeleft', function() {
+		var pageI;
+		if (currentPage == 4) {
+			pageI = 1;
+		} else {
+			pageI = currentPage + 1;
+		}
+		myChangePage(pageI);
+	});
 	//test
 
-	function changePage(index){
-		if(changeStop){
-			changeStop=false;
-			switch(currentPage){
-				case 0:
-					animPage=$("#page0");
-					animPage.addClass("moveoutclass");
-					animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
-					break;
-				case 1:
-					$("#flashBtn").removeClass("mainMenuBtnchoiced");
-					$("#page1").removeClass("moveinclass");
-					animPage=$("#page1");
-					animPage.addClass("moveoutclass");
-					animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
-					break;
-				case 2:
-					$("#androidBtn").removeClass("mainMenuBtnchoiced");
-					$("#page2").removeClass("moveinclass");
-					animPage=$("#page2");
-					animPage.addClass("moveoutclass");
-					animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
-					break;
-				case 3:
-					$("#paintingBtn").removeClass("mainMenuBtnchoiced");
-					$("#page3").removeClass("moveinclass");
-					animPage=$("#page3");
-					animPage.addClass("moveoutclass");
-					animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
-					break;
-				case 4:
-					$("#aboutBtn").removeClass("mainMenuBtnchoiced");
-					$("#page4").removeClass("moveinclass");;
-					animPage=$("#page4");
-					animPage.addClass("moveoutclass");
-					animPage.on("animationend webkitAnimationEnd oAnimationEnd", animationListener);
-					break;
-			}
-			switch(index){
-				case 1:
-					$("#flashBtn").addClass("mainMenuBtnchoiced");
-					$("#page1").show();
-					$("#page1").addClass("moveinclass");
-					break;
-				case 2:
-					$("#androidBtn").addClass("mainMenuBtnchoiced");
-					$("#page2").show();
-					$("#page2").addClass("moveinclass");
-					break;
-				case 3:
-					$("#paintingBtn").addClass("mainMenuBtnchoiced");
-					$("#page3").show();
-					$("#page3").addClass("moveinclass");
-					break;
-				case 4:
-					$("#aboutBtn").addClass("mainMenuBtnchoiced");
-					$("#page4").show();
-					$("#page4").addClass("moveinclass");
-					break;
-			}
-			currentPage=index;
+	function myChangePage(index){
+		switch(currentPage){
+			case 1:
+				$("#flashBtn").removeClass("mainMenuBtnchoiced");
+				break;
+			case 2:
+				$("#androidBtn").removeClass("mainMenuBtnchoiced");
+				break;
+			case 3:
+				$("#paintingBtn").removeClass("mainMenuBtnchoiced");
+				break;
+			case 4:
+				$("#aboutBtn").removeClass("mainMenuBtnchoiced");
+				break;
 		}
-	}
-
-	function animationListener(){
-		animPage.removeClass("moveoutclass");
-		animPage.hide();
-		changeStop=true;
+		if(index<currentPage) {
+			switch (index){
+				case 1:
+					$.mobile.changePage("#page1", {transition: "slide", reverse:true});
+					break;
+				case 2:
+					$.mobile.changePage("#page2", {transition: "slide", reverse:true});
+					break;
+				case 3:
+					$.mobile.changePage("#page3", {transition: "slide", reverse:true});
+					break;
+				case 4:
+					$.mobile.changePage("#page4", {transition: "slide", reverse:true});
+					break;
+			}
+		}else {
+			switch (index){
+				case 1:
+					$.mobile.changePage("#page1", {transition: "slide"});
+					break;
+				case 2:
+					$.mobile.changePage("#page2", {transition: "slide"});
+					break;
+				case 3:
+					$.mobile.changePage("#page3", {transition: "slide"});
+					break;
+				case 4:
+					$.mobile.changePage("#page4", {transition: "slide"});
+					break;
+			}
+		}
+		switch (index){
+			case 1:
+				$("#flashBtn").addClass("mainMenuBtnchoiced");
+				break;
+			case 2:
+				$("#androidBtn").addClass("mainMenuBtnchoiced");
+				break;
+			case 3:
+				$("#paintingBtn").addClass("mainMenuBtnchoiced");
+				break;
+			case 4:
+				$("#aboutBtn").addClass("mainMenuBtnchoiced");
+				break;
+		}
+		currentPage=index;
 	}
 });
